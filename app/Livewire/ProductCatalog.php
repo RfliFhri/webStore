738d325ac8e64ba7feb848a7d31cf49fa5452e0a
RfliFhri <raflifachriansyah43504@gmail.com>
2025-07-18 22:38:25 +0700

    Product Collection data

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -3,18 +3,22 @@ declare(strict_types=1);
 
 namespace App\Livewire;
 
+use App\Data\ProductCollectionData;
 use App\Data\ProductData;
 use App\Models\Product;
+use App\Models\Tag;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
+        $collection_query = Tag::query()->withType('collection')->withCount('products')->get();
         $query = Product::paginate(6);
         // TODO make a DTO
         $products = ProductData::collect($query);
+        $collections = ProductCollectionData::collect($collection_query);
 
-        return view('livewire.product-catalog', compact('products'));
+        return view('livewire.product-catalog', compact('products', 'collections'));
     }
 }
---
+++ app/Models/Tag.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Spatie\Tags\Tag as TagsTag;
+
+class Tag extends TagsTag
+{
+    public function products() : MorphToMany
+    {
+        return $this->morphedByMany(Product::class, 'taggable');
+    }
+}
