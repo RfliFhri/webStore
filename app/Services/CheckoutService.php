f4064bede678ee391d4379bf2300bf2c0bd85fde
RfliFhri <raflifachriansyah43504@gmail.com>
2025-07-27 14:26:54 +0700

    Prepare Output Data Checkout Service

---
+++ app/Services/CheckoutService.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Data\CheckoutData;
+use App\Data\SalesOrderData;
+
+class CheckoutService {
+
+    // public function makeAnOrder(CheckoutData $checkout_data) : SalesOrderData
+    // {
+    //     // TODO : process Checkout
+    //     return new SalesOrderData();
+    // }
+
+}
