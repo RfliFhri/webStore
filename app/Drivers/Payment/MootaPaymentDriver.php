50ca3b1fb73902ed470d2e9b8076ad48d95b97e6
RfliFhri <raflifachriansyah43504@gmail.com>
2025-08-03 21:52:26 +0700

    ubah moota config ke env

--- app/Drivers/Payment/MootaPaymentDriver.php
+++ app/Drivers/Payment/MootaPaymentDriver.php
@@ -27,14 +27,7 @@ class MootaPaymentDriver implements PaymentDriverInterface
     public function getMethods() : DataCollection
     {
         return PaymentData::collect([
-            PaymentData::from([
-                'driver' => $this->driver,
-                'method' => 'bca-bank-transfer',
-                'label' => '(Moota) Bank Transfer BCA',
-                'payload' => [
-                    'account_id' => '3VGjZMRqzrg',
-                ]
-            ])
+            PaymentData::from(config('services.moota.accounts'))
         ], DataCollection::class);
     }
 
